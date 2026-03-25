4ea4ef327a4f9c3062442907706791b5a56ce8bd
Shridhar_Malaghan <24280691@student.ncirl.ie>
2026-03-25 21:46:52 +0000

    added db path

--- smartdine/settings.py
+++ smartdine/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = ["restaurant-booking-app-env.eba-3wzf4qmm.us-east-1.elasticbeanstalk.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["restaurant-booking-app-env.eba-3wzf4qmm.us-east-1.elasticbeanstalk.com","98.83.254.135", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
